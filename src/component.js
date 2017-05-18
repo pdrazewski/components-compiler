2a4bb3289eed2750662745fe3cc3817875dfefdd
przemyslaw.drazewski <przemyslaw.drazewski@enp.pl>
2017-05-18 16:17:08 +0200

    Add compiler files

---
+++ src/component.js
@@ -0,0 +1,4 @@
+(function(){
+    const hello = "Welcome my dark passanger."
+    console.log(hello);
+}())
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const component = require('./package.json');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: [
+  './component.js', 
+  './component.scss'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] },
+        }],
+      },
+      { 
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      { 
+      	test: /\.twig$/, 
+      	loader: "twig-loader" 
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'component.bundle.css',
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+    	title: component.name, 
+    	template: '../template.twig',
+    	minify: {
+    		collapseWhitespace: true
+    	}
+    })
+  ],
+  devServer: {
+    inline: true,
+    port: 8001
+  }
+};
